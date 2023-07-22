2b85a3ea8c2a17dbf029c73a4676d1a17ee7ec33
Mr-Ademis <102387004+Mr-Ademis@users.noreply.github.com>
2023-07-22 23:54:47 +0300

    Update effects.py

--- combat_clas/effects.py
+++ combat_clas/effects.py
@@ -12,21 +12,21 @@ class EffectsPlayer:
     value_bleeding_player: int = 0
     shield: int = 0
 
-    @property
-    def attack_boost_player(self):
+    @property неправильно 
+    def attack_boost_player(self): неправильно
         if self.attack_boost_player > 0:
             return self.value_attack_boost_player
 
-    @property
-    def attack_reduction_player(self):
+    @property неправильно
+    def attack_reduction_player(self): неправильно
         if self.attack_reduction_player > 0:
             return self.value_attack_reduction_player
 
     @property
-    def bleeding_player(self):
+    def bleeding_player(self): неправильно
         if self.bleeding_player > 0:
             return self.value_bleeding_player
 
     @property
-    def shield(self):
+    def shield(self): неправильно
         return self.shield > 0
